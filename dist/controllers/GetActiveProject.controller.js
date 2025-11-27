9b512d45451206e88b0555cb2704d479fbfeb388
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-11-27 13:05:29 +0100

    removed the dist

--- dist/controllers/GetActiveProject.controller.js
+++
@@ -1,9 +0,0 @@
-// import Project from "../models/Project.model";
-// export const getActiveProjects = async (req: Request, res: Response) => {
-//   try {
-//     const projects = await Project.find({ user: req.query.userId, status: "active" });
-//     res.json({ success: true, data: projects });
-//   } catch (err) {
-//     res.status(500).json({ success: false, message: err.message });
-//   }
-// };
